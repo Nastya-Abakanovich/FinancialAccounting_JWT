5cfd03cc8d134ffff93b3b9bff2c6908074c748b
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2023-03-19 01:16:52 +0300

    Add routing.

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Router, Route } from 'react-router-dom';
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
